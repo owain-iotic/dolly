8f62f7566f6ef28a4f4fe54a322a167c4c69313c
jimmy-moore <james.moore@iotics.com>
2021-04-28 11:41:04 +0100

    Send/recv ws working

--- follower/main.go
+++ follower/main.go
@@ -3,31 +3,65 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"golang.org/x/net/websocket"
 )
 
-// Echo the data received on the WebSocket.
-func EchoServer(ws *websocket.Conn) {
+type shipdata struct {
+	lat float64
+	lon float64
+}
+
+var ships = make(map[string]*shipdata)
+
+func ShipServer(ws *websocket.Conn) {
 
 	for {
-		fmt.Printf("Writing data to ws...")
-		ws.Write([]byte("Hello"))
+		// Send all ships...
+		for id, ship := range ships {
+			data := fmt.Sprintf("%s %f %f", id, ship.lat, ship.lon)
+			fmt.Printf("SEND %s\n", data)
+			ws.Write([]byte(data))
+		}
+
+		// Move all ships...
+		MAX_MVT := .02
+		for _, ship := range ships {
+			ship.lat += rand.Float64()*MAX_MVT - rand.Float64()*MAX_MVT
+			ship.lon += rand.Float64()*MAX_MVT - rand.Float64()*MAX_MVT
+
+		}
+
+		fmt.Printf("Waiting...")
 		time.Sleep(1 * time.Second)
 	}
-	//io.Copy(ws, ws)
 }
 
 func main() {
+
+	lat := 52.214016
+	lon := 0.964676
+
+	NUM_SHIPS := 12
+
+	for i := 0; i < NUM_SHIPS; i++ {
+		// Create some lat/lon
+		id := fmt.Sprintf("sh%d", i)
+		ships[id] = &shipdata{
+			lat: lat + rand.Float64()*.1 - rand.Float64()*.1,
+			lon: lon + rand.Float64()*.1 - rand.Float64()*.1,
+		}
+	}
 
 	log.Println("Starting webserver")
 	// Serve static files...
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
 
-	http.Handle("/ws", websocket.Handler(EchoServer))
+	http.Handle("/ws", websocket.Handler(ShipServer))
 
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
