691e1564ac74bcfb6a8e02c1ad85e2a608ee0f8c
jimmy-moore <james.moore@iotics.com>
2021-04-28 10:52:42 +0100

    Started on follower

---
+++ follower/go.mod
@@ -0,0 +1,3 @@
+module github.com/owain-iotic/dolly/follower
+
+go 1.16
---
+++ follower/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("TODO: Start webserver, follow twins, funnel data over a ws to frontend")
+
+}
