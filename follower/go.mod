d5ecaa4fb23f0c0efeeb9d678d5b8097a574daf1
jimmy-moore <james.moore@iotics.com>
2021-04-28 11:23:14 +0100

    Got websocket from go->js started

--- follower/go.mod
+++ follower/go.mod
@@ -1,3 +1,5 @@
 module github.com/owain-iotic/dolly/follower
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20210427231257-85d9c07bbe3a // indirect
--- follower/main.go
+++ follower/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
 )
+
+// Echo the data received on the WebSocket.
+func EchoServer(ws *websocket.Conn) {
+
+	for {
+		fmt.Printf("Writing data to ws...")
+		ws.Write([]byte("Hello"))
+		time.Sleep(1 * time.Second)
+	}
+	//io.Copy(ws, ws)
+}
 
 func main() {
 
@@ -11,6 +26,8 @@ func main() {
 	// Serve static files...
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
+
+	http.Handle("/ws", websocket.Handler(EchoServer))
 
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
